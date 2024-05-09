ba151b1c5cf26469db7d608ed2e0870b6fb57112
Sayed70480 <syedsiknadars4@gmail.com>
2024-05-09 22:03:35 +0800

    updated

--- app/(dashboard)/about/[id].tsx
+++ app/(dashboard)/about/[id].tsx
@@ -1,41 +1,12 @@
 import React from 'react';
 
-export const getStaticPaths = async ()=>{
-    const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-    const data = await res.json();
 
-    const paths = data.map((dataa: any)=>{
-return {
-    params : {id:dataa.id.toString()}
-}
-    })
-
-    return{
-        paths,
-        fallback : false,
-    }
-
-}
-
-
-export const getStaticProps = async (context : any) =>{
-const id  = context.params.id;
-const res = await fetch("https://jsonplaceholder.typicode.com/todos/" + id)
-const data = res.json();
-
-return{
-  props : {ninja : data}
-}
-
-}
-
-
-const AboutIdPage = ({ninja }: {ninja : any}) => {
+const AboutIdPage = () => {
   
 
   return (
     <div>
-      <h1>{ninja.id}</h1>
+     
   
     </div>
   );
